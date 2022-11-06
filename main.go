1f7f89935acc6b0ea901f25b9e9735e32476b7b9
Ikraam <ibndaanis@gmail.com>
2022-11-06 18:32:33 +0000

    basic fiber server

--- main.go
+++ main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
 func main() {
-	println("Hello World")
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	port := ":" + os.Getenv("PORT")
+
+	err := app.Listen(port)
+
+	if err != nil {
+		log.Fatal("There was an error with the server: ", err)
+	}
+
 }
